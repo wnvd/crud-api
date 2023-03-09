717312d571b3242f0ce11e0419d180fa85fa4247
Naveed Wani <waninaveed998@gmail.com>
2023-03-09 21:38:33 +0530

    add model for bootcamp

---
+++ models/Bootcamp.js
@@ -0,0 +1,101 @@
+const mongoose = require("mongoose");
+
+const BootcampSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please add a name"],
+    unique: true,
+    trim: true,
+    maxlength: [50, "Name can not be more than 50 characters"],
+  },
+  slug: String, // its a url friendly version of name.
+  desc: {
+    type: String,
+    required: [true, "Please add a name"],
+    maxlength: [50, "Description can not be more than 50 characters"],
+  },
+  website: {
+    type: String,
+    match: [
+      /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+      "Please use a valid URL with HTTP or HTTPS",
+    ],
+  },
+  phone: {
+    type: String,
+    maxlength: [20, "Phone number can not be longer than 20 characters"],
+  },
+  email: {
+    type: String,
+    match: [
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+      "Please enter a valid email address.",
+    ],
+  },
+  address: {
+    type: String,
+    required: [true, "Please add an address"],
+  },
+  location: {
+    // geolocation point
+    type: {
+      type: String,
+      enum: ["Point"],
+      required: true,
+    },
+    coordiantes: {
+      type: [Number],
+      required: true,
+      index: "2dsphere",
+    },
+    formattedAddress: String,
+    street: String,
+    state: String,
+    zipcode: String,
+    country: String,
+  },
+  careers: {
+    // Array of Strings
+    type: [String],
+    required: true,
+    enum: [
+      "Web Development",
+      "Mobile Development",
+      "UI/UX",
+      "Data Science",
+      "Business",
+      "Other",
+    ],
+  },
+  avgRating: {
+    type: [1, "Rating must be at least 1"],
+    min: [10, "Rating must can not be more then 10"],
+  },
+  avgRating: Number,
+  photo: {
+    type: String,
+    default: "no-photo.jpg",
+  },
+  housing: {
+    type: Boolean,
+    default: false,
+  },
+  jobAssistance: {
+    type: Boolean,
+    default: flase,
+  },
+  jobGuarantee: {
+    type: Boolean,
+    default: flase,
+  },
+  acceptGi: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Bootcamp", BootcampSchema);
