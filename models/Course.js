c9589c10684cbe482406c69096064395921cb77b
Naveed Wani <waninaveed998@gmail.com>
2023-03-20 20:30:57 +0530

    add courses model and data using seeder.js

---
+++ models/Course.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const CourseSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    trime: true,
+    required: [true, "Please add a course title"],
+  },
+  description: {
+    type: String,
+    required: [true, "Please add a descripttion."],
+  },
+  weeks: {
+    type: String,
+    required: [true, "Please add number of weeks"],
+  },
+  tuition: {
+    type: Number,
+    required: [true, "Please add tuition cost"],
+  },
+  minimumSkill: {
+    type: String,
+    required: [true, "Please add Prerequisites"],
+    enum: ["beginner", "intermediate", "advanced"],
+  },
+  scholarshipsAvailable: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  bootcamp: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Bootcamp",
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Course", CourseSchema);
