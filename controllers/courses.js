558c6f7e057d2d4ce24a46b86e0fd551633c58ef
Naveed Wani <waninaveed998@gmail.com>
2023-03-25 19:28:47 +0530

    update and delete course functionality

--- controllers/courses.js
+++ controllers/courses.js
@@ -70,3 +70,47 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
     data: course,
   });
 });
+
+// @desc Update course
+// @route PUT /api/v1/course/:id
+// @access Private
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+  let course = await Course.findById(req.params.id);
+
+  if (!course) {
+    return next(
+      new ErrorResponse(`No course with the Id if ${req.params.id}`),
+      404,
+    );
+  }
+  course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: course,
+  });
+});
+
+// @desc Delete course
+// @route DELETE /api/v1/course/:id
+// @access Private
+exports.deleteCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id);
+
+  if (!course) {
+    return next(
+      new ErrorResponse(`No course with the Id if ${req.params.id}`),
+      404,
+    );
+  }
+  await course.deleteOne();
+
+  res.status(200).json({
+    success: true,
+    msg: "Course has been deleted from the database",
+    data: {},
+  });
+});
