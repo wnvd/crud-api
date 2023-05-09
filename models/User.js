15565049ead64617a5293b7ae72d9d5d700d6701
Naveed Wani <waninaveed998@gmail.com>
2023-05-09 14:59:25 +0530

    add User model

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add a name'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Please add an email'],
+        unique: true,
+        match: [
+            /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+        "Please enter a valid email address.",
+        ] 
+    },
+    role: {
+        type: String,
+        enum: ['user', 'publisher'],
+        default: 'user'
+    },
+    password: {
+        type: String,
+        required: [true, 'Please add a password'],
+        minlength: 6,
+        select: false,
+    },
+   resetPasswordToken: String,
+   resetPasswordExpire: Date,
+   createdAt: {
+    type: Date,
+    default: Date.now
+   }
+
+});
+
+module.exports = mongoose.model('User', UserSchema);
