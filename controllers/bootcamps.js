29fcf8f89892f43afb9c7fb86b804ff59b9a51e2
Naveed Wani <waninaveed998@gmail.com>
2023-05-13 16:36:31 +0530

    link user model to bootcamp model, now user id is con with  bootcamp added

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -49,6 +49,15 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
  * @access Private
  */
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
+    // Add user to req.body
+    req.body.user = req.user.id;
+    
+    // Check for published bootcamps
+    const publishedBootcamp = await Bootcamp.findOne({ user: req.user.id }); 
+    // if the user is not an admin, they can only add one bootcamp
+    if(publishedBootcamp && req.user.role !== 'admin') {
+        return next( new ErrorResponse(`The user with Id ${req.user.id} has already published a bootcamp`, 400));
+    }
 	const bootcamp = await Bootcamp.create(req.body);
 	res.status(201).json({
 		success: true,
