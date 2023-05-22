bbc820bab904a135d93d7ac9a55dca2afcdffccd
Naveed Wani <waninaveed998@gmail.com>
2023-05-22 17:06:57 +0530

    update and delete review functionality with protect and authorize middleware

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -64,3 +64,64 @@ exports.addReview = asyncHandler(async (req, res, next) => {
         data: review
     })
 });
+
+/* @desc    Update a review 
+ * @route   POST /api/v1/reviews/:id
+ * @access  Private
+ */
+exports.updateReview = asyncHandler(async (req, res, next) => {
+
+    let review = await Review.findById(req.params.id);
+
+    if(!review){
+        return next(
+            new ErrorResponse(`No review of ID ${req.params.id} exists`, 404)
+        );
+    }
+    
+    // Make sure review belongs to user or user in admin
+    if(review.user.toString() !== req.user.id 
+        && req.user.role !== 'admin') {
+        return next(
+            new ErrorResponse(`Not authorized to update review`, 401)
+        );
+    }
+
+    review = await Review.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    }); 
+    res.status(201).json({
+        success: true,
+        data: review
+    })
+});
+
+/* @desc    Delete a review 
+ * @route   DELETE /api/v1/reviews/:id
+ * @access  Private
+ */
+exports.deleteReview = asyncHandler(async (req, res, next) => {
+
+    const review = await Review.findById(req.params.id);
+
+    if(!review){
+        return next(
+            new ErrorResponse(`No review of ID ${req.params.id} exists`, 404)
+        );
+    }
+    
+    // Make sure review belongs to user or user in admin
+    if(review.user.toString() !== req.user.id 
+        && req.user.role !== 'admin') {
+        return next(
+            new ErrorResponse(`Not authorized to update review`, 401)
+        );
+    }
+
+    await review.deleteOne(review);
+    res.status(201).json({
+        success: true,
+        data: {}
+    })
+});
