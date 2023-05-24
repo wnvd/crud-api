0055765879d7c4e04778ab8f73a7cb78ae7089a2
Naveed Wani <waninaveed998@gmail.com>
2023-05-24 15:55:38 +0530

    add logout route and clear cookies when logout

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,18 @@
 const express = require("express");
-const { register, login, getMe, forgotPassword, resetPassword, updateDetails, updatePassword } = require("../controllers/auth");
+const { register, 
+        login, 
+        getMe,  
+        logout,  
+        forgotPassword, 
+        resetPassword, 
+        updateDetails, 
+        updatePassword } = require("../controllers/auth");
 const { protect } = require("../middleware/auth");
 const router = express.Router();
 
 router.post("/register", register);
 router.get("/login", login);
+router.get("/logout", logout)
 router.get("/me", protect, getMe);
 router.put("/updatedetails", protect, updateDetails);
 router.put("/updatepassword", protect, updatePassword);
